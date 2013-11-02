26dc530bb2d9fc3aa76a52fe0cb1612265436949
ishitcno1 <ishitcno1@gmail.com>
2013-11-02 18:28:05 +0800

    Update the ration from 6 to 18.

--- Readme.md
+++ Readme.md
@@ -6,7 +6,7 @@
 <span id="resize_image"></span>
 Resize image to xxhdpi xhdpi hdpi mdpi.
 
-The origin image is designed in ndp * 6 px. For example:
-an icon(48dp * 48dp) will be designed in 288px * 288px.
+The origin image is designed in ndp * 18 px. For example:
+an icon(48dp * 48dp) will be designed in 864px * 864px.
 Then the script will generate images in xxhdpi
 xhdpi hdpi and mdpi.
--- resize_img.py
+++ resize_img.py
@@ -1,7 +1,7 @@
 """Resize image to xxhdpi xhdpi hdpi mdpi.
 
-The origin image is designed in ndp * 6 px size. For example:
-an icon(48dp * 48dp) will be designed in 288px * 288px.
+The origin image is designed in ndp * 18 px size. For example:
+an icon(48dp * 48dp) will be designed in 864px * 864px.
 With these images, the script will generate images in xxhdpi
 xhdpi hdpi and mdpi.
 
@@ -10,8 +10,8 @@ xhdpi hdpi and mdpi.
 from PIL import Image
 import os
 
-size_info = {'xxhdpi': 2, 'xhdpi': 3, 'hdpi': 4, 
-	'mdpi': 6}
+size_info = {'xxhdpi': 6, 'xhdpi': 9, 'hdpi': 12, 
+	'mdpi': 18}
 
 # Get images and remove script
 images = os.listdir('.')
