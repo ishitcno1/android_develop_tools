5b55d4a11c3011edb2ba36fc6cca2efeae9ed5ef
ishitcno1 <ishitcno1@gmail.com>
2013-11-05 22:11:10 +0800

    fix some bugs and add resize_img_ratio6.py

--- Readme.md
+++ Readme.md
@@ -6,7 +6,10 @@
 <span id="resize_image"></span>
 Resize image to xxhdpi xhdpi hdpi mdpi.
 
-The origin image is designed in ndp * 18 px. For example:
+The original image is designed in ndp * 18 px. For example:
 an icon(48dp * 48dp) will be designed in 864px * 864px.
 Then the script will generate images in xxhdpi
 xhdpi hdpi and mdpi.
+
+resize_img_ratio6.py is similar, but the original image is
+designed in ndp * 6 px.
--- resize_img_ratio6.py
+++ resize_img_ratio6.py
@@ -1,7 +1,7 @@
 """Resize image to xxhdpi xhdpi hdpi mdpi.
 
-The origin image is designed in ndp * 18 px size. For example:
-an icon(48dp * 48dp) will be designed in 864px * 864px.
+The origin image is designed in ndp * 6 px size. For example:
+an icon(48dp * 48dp) will be designed in 288px * 288px.
 With these images, the script will generate images in xxhdpi
 xhdpi hdpi and mdpi.
 
@@ -10,12 +10,12 @@ xhdpi hdpi and mdpi.
 from PIL import Image
 import os
 
-size_info = {'xxhdpi': 6, 'xhdpi': 9, 'hdpi': 12, 
-	'mdpi': 18}
+size_info = {'xxhdpi': 2, 'xhdpi': 3, 'hdpi': 4, 
+	'mdpi': 6}
 
 # Get images and remove script
 images = os.listdir('.')
-images.remove('resize_img.py')
+images.remove('resize_img_ratio6.py')
 
 # Create directories
 for dir_name in size_info.keys():
